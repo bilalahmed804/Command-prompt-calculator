8e0de2088a0243517dd9fa4b21062782e3e50a18
Bilal Ahmed <ba3801534@gmail.com>
2024-04-01 00:23:57 +0500

    Command line Calculator

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "Subtraction", "Multiplication", "Division"]
+  },
+]);
+
+
+
+//Conditinal statement
+
+if(answer.operator === "addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+}else if(answer.operator === "Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+}else if(answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber);
+}else if(answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+}else{
+    console.log("please select valid operator");
+    
+}
